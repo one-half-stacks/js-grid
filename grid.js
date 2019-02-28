e1e7a3f3740e173cd0b90bf7c495a38c9554398d
Sam Yong <yongmc.sam2014@outlook.com>
2019-02-28 15:32:48 -0500

    draw grid

--- grid.js
+++ grid.js
@@ -1,8 +1,8 @@
 jsGrid(document.getElementById('grid-container'), {
   // essential config vars
   cellSize: 50, // px
-  rows: 10,
-  cols: 20,
+  rows: 12,
+  cols: 24,
   grid: { // element class
     cell: 'grid-cell',
     fixL: 'fl', // left border is fixed border
@@ -14,7 +14,23 @@ jsGrid(document.getElementById('grid-container'), {
 
 function jsGrid(ele, config, rules) {
 
+  drawGrid();
+
   function drawGrid() {
+    var html = ele.innerHTML;
+    for (var i = 0; i < config.rows; i++) {
+      for (var j = 0; j < config.cols; j++) {
+        html += "<div class='grid-cell' id='gcell-" + i + "-" + j + "'></div>";
+      }
+    }
+    ele.innerHTML = html;
+    for (var i = 0; i < config.rows; i++) {
+      for (var j = 0; j < config.cols; j++) {
+        var c = document.getElementById('gcell-' + i + '-' + j);
+        c.style.top = i * config.cellSize + 'px';
+        c.style.left = j * config.cellSize + 'px';
+      }
+    }
   }
 
 }
