dfad6f083fd81ac4b9c0363f44d71839f1f2e851
Sam Yong <yongmc.sam2014@outlook.com>
2019-02-28 13:18:42 -0500

    grid config

--- grid.js
+++ grid.js
@@ -1,4 +1,20 @@
-jsGrid(document.getElementById('grid-container'));
+jsGrid(document.getElementById('grid-container'), {
+  // essential config vars
+  cellSize: 50, // px
+  rows: 10,
+  cols: 20,
+  grid: { // element class
+    cell: 'grid-cell',
+    fixL: 'fl', // left border is fixed border
+    fixR: 'fr',
+  },
+}, {
+  // layout rules
+});
 
-function jsGrid(ele) {
+function jsGrid(ele, config, rules) {
+
+  function drawGrid() {
+  }
+
 }
